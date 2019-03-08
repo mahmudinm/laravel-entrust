ca01981bad64170abb7906c7754356eb2c9f8453
mahmudin muttaqin <claxmanx@gmail.com>
2019-03-08 15:38:00 +0700

    tambah comment

--- resources/views/role.blade.php
+++ resources/views/role.blade.php
@@ -8,10 +8,12 @@
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
+                    {{-- Jika admin bisa akses --}}
                     @role('admin')
                         <h1>Hi Admin</h1>
                     @endrole
 
+                    {{-- Jika admin atau user bisa akses --}}
                     @role(['user', 'admin'])
                         <h1>Hi user</h1>
                     @endrole
--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,16 @@ Route::get('/', function () {
 Auth::routes();
 
 
+// Jika admin bisa akses halaman
 Route::get('/admin', function(){
 	echo 'oh hello admin';
 })->middleware(['auth', 'role:admin']);
 
+// Jika user atau admin bisa akses halaman
 Route::get('/user', function(){
 	echo 'oh hello user';
 })->middleware(['auth', 'role:user|admin']);
+
 
 Route::get('/role', function(){
 	return view('role');
